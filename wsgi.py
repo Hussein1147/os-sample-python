7921880c9ebd09313af085e9a91fc944822bf71d
Djibril <Djibrilhms@gmail.com>
2018-05-27 18:10:03 -0400

    chnages

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World dear friend"
+    return "Hello World dear "
 
 if __name__ == "__main__":
     application.run()
